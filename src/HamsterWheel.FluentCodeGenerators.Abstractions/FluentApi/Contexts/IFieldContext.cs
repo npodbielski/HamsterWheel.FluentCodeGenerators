7a56df33ccd25aba262916f9665c8b0a0eb10ede
natan <natan@podbielski.it>
2025-09-29 10:02:13 +0200

    feat(fluentGenerators): push FieldContext methods to IFieldContext.cs

--- src/HamsterWheel.FluentCodeGenerators.Abstractions/FluentApi/Contexts/IFieldContext.cs
+++ src/HamsterWheel.FluentCodeGenerators.Abstractions/FluentApi/Contexts/IFieldContext.cs
@@ -1,7 +1,12 @@
 namespace HamsterWheel.FluentCodeGenerators.FluentApi.Contexts;
 
-public interface IFieldContext : ITypeMemberWithType<IFieldContext>, ITypeMember<IFieldContext>,
-    IMemberWithAccessModifier<IFieldContext>, IContext
+public interface IFieldContext : ITypeMemberWithType<IFieldContext>, 
+    ITypeMember<IFieldContext>,
+    IMemberWithAccessModifier<IFieldContext>,
+    IContext
 {
     IFieldContext Named(string name);
+    IFieldContext MakeNullable();
+    IFieldContext DisableNullabilityWarning();
+    IFieldContext WithInitializer(string value);
 }
